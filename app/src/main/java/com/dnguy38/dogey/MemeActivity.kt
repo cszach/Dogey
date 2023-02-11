9ae2c1f10f44ea8129aba558b94f5174ea43a805
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-02-11 00:12:49 -0500

    Test image cute cat

--- app/src/main/java/com/dnguy38/dogey/MemeActivity.kt
+++ app/src/main/java/com/dnguy38/dogey/MemeActivity.kt
@@ -2,10 +2,28 @@ package com.dnguy38.dogey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MemeActivity : AppCompatActivity() {
+    private lateinit var memeImageView: ImageView
+    private lateinit var memeTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meme)
+
+        memeImageView = findViewById(R.id.memeImageView)
+        memeTextView = findViewById(R.id.memeTextView)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val memeImageId = bundle.getInt(MEME_IMAGE_ID)
+            val memeText = bundle.getString(MEME_TEXT)
+
+            val memeImage = getDrawable(memeImageId)
+
+            memeImageView.setImageDrawable(memeImage)
+        }
     }
 }
