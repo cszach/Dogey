231ae6a03b5576639148259cb6a318bb7876ed70
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-02-10 23:05:27 -0500

    Initialize (empty) meme activity

---
+++ app/src/main/java/com/dnguy38/dogey/MemeActivity.kt
@@ -0,0 +1,11 @@
+package com.dnguy38.dogey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meme)
+    }
+}
