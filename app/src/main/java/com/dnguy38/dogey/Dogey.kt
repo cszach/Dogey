037b55a6f51ccad4c7e2dafa10d3beac0d3398af
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-02-10 23:42:46 -0500

    Main activity working (w/ snackbar)

---
+++ app/src/main/java/com/dnguy38/dogey/Dogey.kt
@@ -0,0 +1,9 @@
+package com.dnguy38.dogey
+
+class Dogey {
+    var count: Int = 0
+
+    fun addCount() {
+        count++
+    }
+}
--- app/src/main/java/com/dnguy38/dogey/MainActivity.kt
+++ app/src/main/java/com/dnguy38/dogey/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.dnguy38.dogey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_SHORT
+import com.google.android.material.snackbar.Snackbar
+
+const val MEME_IMAGE_ID = "com.dnguy38.dogey.meme_image"
+const val MEME_TEXT = "com.dnguy38.dogey.meme_text"
 
 class MainActivity : AppCompatActivity() {
+    private val model: Dogey = Dogey()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        Snackbar.make(findViewById(R.id.actionButton), "${model.count}", LENGTH_SHORT).show()
+    }
+
+    fun onGenerateMeme(view: View) {
+        val intent = Intent(this, MemeActivity::class.java)
+        val memeImageId = R.drawable.cute_cat
+        val memeText = getText(R.string.app_name)
+
+        intent.putExtra(MEME_IMAGE_ID, memeImageId)
+        intent.putExtra(MEME_TEXT, memeText)
+
+        startForResult.launch(intent)
+        model.addCount()
     }
 }
